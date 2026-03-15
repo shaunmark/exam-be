ac907b7e26c8c6e97b1fed84c427006ab22e8223
shaunmark <shaunantony1000@gmail.com>
2026-03-15 18:27:27 +0530

    made be server to be open to all ports for raleway reployment

--- src/main.ts
+++ src/main.ts
@@ -39,6 +39,7 @@ async function bootstrap() {
   app.enableCors();
 
   /** Listen on PORT from env, or default to 3000. */
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0'); //for railway
 }
 void bootstrap();
